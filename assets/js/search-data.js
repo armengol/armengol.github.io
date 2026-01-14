33b14262e03763a2cc6fa46bf14eb07efd5a4c42
armengol <alex_a_u@msn.com>
2026-01-14 17:42:16 +0000

    Deploying to gh-pages from @ armengol/armengol.github.io@4a8aa705043a5738900606c0bead852429753c29 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -461,13 +461,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/alex-armengol-urpi", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
