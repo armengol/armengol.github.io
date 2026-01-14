47253adfb880bdf104f60cf12e15c2c33c90b3f9
armengol <alex_a_u@msn.com>
2026-01-14 15:19:54 +0000

    Deploying to gh-pages from @ armengol/armengol.github.io@12607b307fe0e988fb454c151e82d055524f9d9b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
+  },{id: "nav-others",
+          title: "others",
+          description: "Things I enjoy working on and exploring in my free time.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/others/";
+          },
+        },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
